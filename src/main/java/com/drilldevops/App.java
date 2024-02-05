df54afe0697f937d41eefc966cf5c01787264a0f
kalyanreddyc <kalyanreddychitiki@gmail.com>
2024-02-05 06:55:25 +0530

    updating first-demo-project-that produces jar package

---
+++ src/main/java/com/drilldevops/App.java
@@ -0,0 +1,13 @@
+package com.drilldevops;
+
+/**
+ * Hello DrillDevOps Team!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello DrillDevOps Team! Happy Learning" );
+    }
+}
